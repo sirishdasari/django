1793682636af3f2a44e63453f470308d9d329dfb
dasarisirish <dasarisirish@gmail.com>
2023-03-12 15:55:59 +0000

    env added

--- project/settings.py
+++ project/settings.py
@@ -74,23 +74,23 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'sirishdjango',
-        'USER': 'sirishdasari',
-        'PASSWORD': 'sirishdas',
-        'HOST': '139.59.59.192',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'sirishdjango',
+#         'USER': 'sirishdasari',
+#         'PASSWORD': 'sirishdas',
+#         'HOST': '139.59.59.192',
+#         'PORT': '',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
